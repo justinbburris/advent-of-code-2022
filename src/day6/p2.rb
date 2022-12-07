22d7a5f53ff2af24b18f6c94daf2bc5245efc4e4
Justin Burris <713795+justinbburris@users.noreply.github.com>
2022-12-06 23:07:58 -0500

    sliding solution

--- src/day6/p2.rb
+++ src/day6/p2.rb
@@ -6,7 +6,13 @@ signal.chars.each.with_index(1) do |c, i|
   packet += c
   location = i
   packet = packet[1..-1] if packet.length == 15
-  return if packet.chars.uniq.length == 14
+  break if packet.chars.uniq.length == 14
 end
-
 puts location
+
+# sliding window solution
+location = 14
+(0..signal.length).each do |i|
+  location = i + 14
+  break if signal[i...location].chars.uniq.length == 14
+end
